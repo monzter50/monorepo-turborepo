81bc03cf579f928d120230f7309ed2fe04859d01
monzter50 <josemonzter50@gmail.com>
2021-12-21 08:37:22 -0500

    feat(components): create new component and implement conf

---
+++ src/components/atoms/Badges/Badges.stories.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { Badges } from './index'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Badges',
+  component: Badges,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Badges {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  label: 'Badges'
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  typeBadges: 'secondary',
+  label: 'Badges'
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+  label: 'Badges'
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  label: 'Badges'
+}
+
+export const Link = Template.bind({})
+Link.args = {
+  size: 'small',
+  label: 'Badges'
+}
---
+++ src/components/atoms/Badges/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './badges.css'
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Badges = ({ typeBadges, backgroundColor, size, label, ...props }) => {
+  const mode = {
+    electric: 'storybook-bandges--electric',
+    poison: 'storybook-bandges--poison',
+    ground: 'storybook-bandges--ground'
+  }
+  return (
+    <div
+      role="badges"
+      className={['storybook-badges', `storybook-badges--${size}`, mode[typeBadges]].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </div>
+  )
+}
+
+Badges.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  typeBadges: PropTypes.oneOf(['electric', 'posion', 'ground', 'normal']),
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func
+}
+
+Badges.defaultProps = {
+  typeBadges: 'normal',
+  backgroundColor: null,
+  size: 'medium',
+  onClick: undefined
+}
