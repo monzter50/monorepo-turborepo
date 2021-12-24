3218c56388ad6da4ed98e89eda25829342e75548
monzter50 <josemonzter50@gmail.com>
2021-12-24 04:14:26 -0500

    feat(components): add styles and estructure card

--- src/components/molecules/Card/index.jsx
+++ src/components/molecules/Card/index.jsx
@@ -1,48 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './button.css'
+import { Card } from './Card'
+import './card.css'
 
 /**
  * Primary UI component for user interaction
  */
-export const Card = ({ theme, backgroundColor, size, children, ...props }) => {
-  return (
-    <div
-      className={['storybook-card', `storybook-card--${size}`, `storybook-card--${theme}`].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {children}
-    </div>
-  )
-}
-
-Card.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  theme: PropTypes.oneOf(['default', 'dark', 'light']),
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  children: PropTypes.node.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func
-}
-
-Card.defaultProps = {
-  backgroundColor: null,
-  typeButton: 'primary',
-  size: 'medium',
-  onClick: undefined
-}
+export { Card }
