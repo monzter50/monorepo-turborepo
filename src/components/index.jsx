09e72265efcc1e56bb4204977cba0423885d64c9
monzter50 <josemonzter50@gmail.com>
2021-12-26 12:47:57 -0500

    feat(structure): structure files

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import { Badges } from './atoms/Badges'
+import { Button } from './atoms/Button'
+import { Skeleton } from './atoms/Skeleton'
+import { Card } from './molecules/Card'
+
+export { Badges, Button, Skeleton, Card }
